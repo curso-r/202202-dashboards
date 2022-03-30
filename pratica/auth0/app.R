cbe351ecba9c2edbaff4a2f17a90929cbbe7cf38
williamorim <thewilliam89@gmail.com>
2022-03-29 22:18:32 -0300

    aula 6

---
+++ pratica/auth0/app.R
@@ -0,0 +1,83 @@
+library(shiny)
+library(dplyr)
+library(auth0)
+library(shinyjs)
+
+dados <- readr::read_rds("pkmn.rds")
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        "geracao",
+        label = "Selecione uma geração",
+        choices = na.exclude(unique(dados$id_geracao))
+      ),
+      selectInput(
+        "pokemon",
+        label = "Selecione um pokemon",
+        choices = c("Carregando..." = "")
+      ),
+      auth0::logoutButton()
+    ),
+    mainPanel(
+      fluidRow(
+        column(
+          offset = 4,
+          width = 4,
+          uiOutput("pkmn")
+        )
+      )
+    )
+  )
+)
+
+server <- function(input, output, session) {
+
+  observe({
+    opcoes <- dados %>%
+      filter(id_geracao == input$geracao) %>%
+      pull(pokemon)
+
+    updateSelectInput(
+      session,
+      inputId = "pokemon",
+      choices = opcoes,
+      selected = opcoes[1] #já é o padrão selecionar a primeira
+    )
+
+  })
+
+  output$pkmn <- renderUI({
+
+    req(input$pokemon)
+
+    id <- dados %>%
+      filter(pokemon == input$pokemon) %>%
+      pull(id) %>%
+      stringr::str_pad(
+        width = 3,
+        side = "left",
+        pad = "0"
+      )
+
+    url <- glue::glue(
+      "https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/{id}.png"
+    )
+
+    tags$img(
+      src = url,
+      width = "300px",
+      alt = glue::glue("Quem é esse pokemon? É o {input$pokemon}.")
+    )
+
+  })
+
+}
+
+auth0::shinyAppAuth0(
+  ui,
+  server,
+  config_file = "_auth0.yml",
+  options = list(port = 8080)
+)
